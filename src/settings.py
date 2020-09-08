9ec2ea1a2f5e72bab975f376b303148e32c48e53
M.Dulchevskiy <mdulchevskiy@gmail.com>
2020-09-08 13:20:06 +0300

    Fix some naming issues.

--- src/settings.py
+++ src/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_for_deploy')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
